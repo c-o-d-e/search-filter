0aea121fcdb42cfab137808f4f6723d276908caf
c-o-d-e <swapyuk@gmail.com>
2022-08-19 21:54:04 +0100

    filter added to the json data and onChange input

--- src/App.js
+++ src/App.js
@@ -2,12 +2,37 @@ import React, { useState } from "react";
 import jsondata from "./mock-data.json";
 
 function App() {
+    const [searchTerm, setSearchTerm] = useState("");
     return (
         <main className="App">
-            <input type="text" placeholder="Search term..." />
-            {jsondata.map((val, key) => {
-                return <h1 className="jsonNames">{val.first_name}</h1>;
-            })}
+            <input
+                type="text"
+                placeholder="Search term..."
+                onChange={event => {
+                    setSearchTerm(event.target.value);
+                }}
+            />
+            {jsondata
+                .filter(val => {
+                    if (searchTerm == "") {
+                        return val;
+                    } else if (
+                        val.first_name
+                            .toLowerCase()
+                            .includes(searchTerm.toLowerCase())
+                    ) {
+                        return val;
+                    } else {
+                        return;
+                    }
+                })
+                .map((val, key) => {
+                    return (
+                        <h1 className="jsonNames" key={key}>
+                            {val.first_name}
+                        </h1>
+                    );
+                })}
         </main>
     );
 }
