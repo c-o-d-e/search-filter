13924470469a0c33e0608b4db40079c17b6668ab
c-o-d-e <swapyuk@gmail.com>
2022-08-19 21:37:12 +0100

    jsondata mapped using h1 jsx

--- src/App.js
+++ src/App.js
@@ -1,10 +1,15 @@
 import React, { useState } from "react";
+import jsondata from "./mock-data.json";
 
 function App() {
-    return <main className="App">
-        <input type="text" placeholder="Search term..."/>
-        
-    </main>;
+    return (
+        <main className="App">
+            <input type="text" placeholder="Search term..." />
+            {jsondata.map((val, key) => {
+                return <h1 className="jsonNames">{val.first_name}</h1>;
+            })}
+        </main>
+    );
 }
 
 export default App;
